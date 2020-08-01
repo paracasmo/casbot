6840a0a9e2fda735cb203f7dd0a71581b44b9aa4
Ruben Steinman <ruben.steinman@adesso.de>
2020-08-01 20:01:42 +0200

    add db for.. later use i guess

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const winston = require('winston')
-
+const winston = require("winston");
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.cli(),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    ),
-    transports: [new winston.transports.Console()]
-})
+  level: "info",
+  format: winston.format.combine(
+    winston.format.cli(),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  ),
+  transports: [new winston.transports.Console()],
+});
+const FileSync = require("lowdb/adapters/FileSync");
+const db = require("lowdb")(new FileSync("db.json"));
 
-logger.warn('LIT')
-logger.info('HUA')
-logger.error('NIA')
+logger.warn("LIT");
+logger.info("HUA");
+logger.error("NIA");
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ commands: [] }).write();
+db.set("commands", []).write();
+db.get("commands").push({ id: "coolCommand1" }).write();
+logger.info(`db contents: ${db.get("commands").map("id").value()}`);
